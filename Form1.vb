b0cccf3e9cd4ce14171a4e0a18391726944ffa70
Yvonne Santiago <ybsantiago@iskolarngbayan.pup.edu.ph>
2022-06-11 01:37:24 +0800

    Display the answers in text file

--- Form1.vb
+++ Form1.vb
@@ -93,4 +93,8 @@
         writefile.Close()
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Application.Exit()
+    End Sub
 End Class
